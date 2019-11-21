3ad6222cd42f1b677a9a458b6d54a6617b29d48a
tkato <2367707+tkato-89024@users.noreply.github.com>
2019-11-21 17:01:43 +0900

    追加 InstrumentedTest のベースクラス

---
+++ general-module/src/androidTest/java/jp/co/model/tkato/general_module/base/BaseInstrumentedTest.java
@@ -0,0 +1,34 @@
+package jp.co.model.tkato.general_module.base;
+
+import java.util.concurrent.CountDownLatch;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class BaseInstrumentedTest {
+
+    private CountDownLatch latch;
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    public CountDownLatch countSetup(int count) {
+        latch = new CountDownLatch(count);
+        return latch;
+    }
+
+    public void tearDown() throws Exception {
+        countFinish();
+        latch = null;
+    }
+
+    protected final void countFinish() {
+        if (null == latch) {
+            return;
+        }
+        final int len = (int) latch.getCount();
+        for (int i = 0; i < len; i++) {
+            latch.countDown();
+        }
+    }
+}
